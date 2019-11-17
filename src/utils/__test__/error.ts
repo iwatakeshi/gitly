2ffaa69734d316adb53fd9f930eae1c482acfd89
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-16 20:14:19 -0700

    Add tests for error types

---
+++ src/utils/__test__/error.ts
@@ -0,0 +1,31 @@
+import { GitlyDownloadError, GitlyErrorType, GitlyExtractError, GitlyFetchError } from '../error'
+
+describe('utils/error', () => {
+
+  describe('GitlyFetchError', () => {
+    it('should return an instance', () => {
+      const error = new GitlyFetchError('message')
+      expect(error.message).toBe('[gitly:fetch]: message')
+      expect(error.code).toBe(-1)
+      expect(error.type).toBe(GitlyErrorType.Fetch)
+    })
+  })
+
+  describe('GitlyExtractError', () => {
+    it('should return an instance', () => {
+      const error = new GitlyExtractError('message')
+      expect(error.message).toBe('[gitly:extract]: message')
+      expect(error.code).toBe(-1)
+      expect(error.type).toBe(GitlyErrorType.Extract)
+    })
+  })
+
+  describe('GitlyDownloadError', () => {
+    it('should return an instance', () => {
+      const error = new GitlyDownloadError('message', 402)
+      expect(error.message).toBe('[gitly:download]: message')
+      expect(error.code).toBe(402)
+      expect(error.type).toBe(GitlyErrorType.Download)
+    })
+  })
+})

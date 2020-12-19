010550e7cdf2ce8f1aa1de38dad605f16eac4116
Takeshi Iwana <tak@prismify.co>
2020-12-19 13:44:20 -0700

    Added new API

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
-export { default as fetch } from './utils/fetch'
+export { default } from './utils/gitly'
+export { default as download } from './utils/download'
 export { default as extract } from './utils/extract'
 export { default as parse } from './utils/parse'
---
+++ src/utils/gitly.ts
@@ -0,0 +1,19 @@
+import GitlyOptions from '../interfaces/options'
+import download from './download'
+import extract from './extract'
+
+/**
+ * Downloads and extracts the repository
+ * @param repository The repository to download
+ * @param destination The destination to extract
+ * @param options
+ * @returns A tuple with the source and destination respectively
+ */
+export default async function gitly(
+  repository: string,
+  destination: string,
+  options: GitlyOptions
+): Promise<[string, string]> {
+  const source = await download(repository, options)
+  return [source, await extract(source, destination, options)]
+}

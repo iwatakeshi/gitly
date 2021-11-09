43a585d3d71d769ec7a3601cafd96e0e875a1e62
Takeshi <iwatakeshi@gmail.com>
2021-11-09 16:36:00 -0700

    Remove previous code

--- src/utils/is-offline.ts
+++
@@ -1,9 +0,0 @@
-import { promises as dns } from 'dns'
-export default async function isOffline(): Promise<boolean> {
-  try {
-    await dns.lookup('google.com')
-    return false
-    // eslint-disable-next-line no-empty
-  } catch (_) {}
-  return true
-}

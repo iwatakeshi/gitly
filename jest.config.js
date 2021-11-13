5eb5f5620b6a3cbc93db4f7dec752f27f733dd03
Takeshi <iwatakeshi@gmail.com>
2021-11-12 21:16:13 -0700

    Add babel configuration for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      babelConfig: {
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-transform-runtime']
+      }
+    }
+  }
 };

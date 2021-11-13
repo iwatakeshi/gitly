0d60243d4d4e8ce105ebdd2d2d5e3c911d511472
Takeshi <iwatakeshi@gmail.com>
2021-11-13 12:04:12 -0700

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { GitlyOptions } from './core/gitly'
-
-export { default as gitly } from './core/gitly'
+export { default } from './core/gitly'
+export * from './core/gitly'
 export * from './core/download'
 export * from './core/extract'
 export * from './utils/url/parse'
 export * from './utils/url/git-url'
 export * from './types/git'
-export { GitlyOptions }

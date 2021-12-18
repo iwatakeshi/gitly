2f739e7b3e67da5db14b0281d58d64cdbefd786f
Takeshi Iwana <tak@prismify.co>
2021-12-18 16:02:57 -0700

    Add new signatures for function overloading gitly.

--- src/core/gitly/index.ts
+++ src/core/gitly/index.ts
@@ -12,23 +12,52 @@ export interface GitlyOptions extends GitlyDownloadOptions {
   extract?: ExtractOptions
 }
 
+export type GitlyResult = [source: string, destination: string]
+
+
 /**
- * Downloads and extracts the repository
- * @param repository The repository to download
- * @param destination The destination path to download the repository
- * @param options The options for gitly
- * @returns A tuple with the source and destination respectively
+ * Downloads and extracts the repository.
+ * @param repository The repository to download.
+ * @returns A promise containing a tuple with the source and destination respectively.
  */
-export default async function gitly(
-  repository: string,
-  destination: string = GITLY_PATH,
-  options: GitlyOptions = {}
-): Promise<[string, string]> {
-  return pipeAsync<[string, string]>(
+export async function gitly(repository: string): Promise<GitlyResult>;
+/**
+ * Downloads and extracts the repository.
+ * @param repository The repository to download.
+ * @param destination The destination path to download the repository.
+ * @returns A promise containing a tuple with the source and destination respectively.
+ */
+export async function gitly(repository: string, destination: string): Promise<GitlyResult>;
+/**
+ * 
+ * @param repository The repository to download.
+ * @param options The options for gitly.
+ * @returns A promise containing a tuple with the source and destination respectively.
+ */
+export async function gitly(repository: string, options: GitlyOptions): Promise<GitlyResult>;
+/**
+ * 
+ * @param repository The repository to download.
+ * @param destination The destination path to download the repository.
+ * @param options The options for gitly.
+ * @returns A promise containing a tuple with the source and destination respectively.
+ */
+export async function gitly(repository: string, destination: string, options: GitlyOptions): Promise<GitlyResult>;
+
+export async function gitly(
+  a: string,
+  b?: string | GitlyOptions,
+  c?: GitlyOptions
+): Promise<any> {
+  const destination = typeof b === 'string' ? b : GITLY_PATH
+  const options = (typeof b === 'object' ? b : c) || {}
+  return pipeAsync<GitlyOptions>(
     $download(options),
     tapAsync(
       $extract(destination, { ...options?.extract, throw: options.throw })
     ),
     (source: string) => [source, destination]
-  )(repository)
+  )(a)
 }
+
+export default gitly

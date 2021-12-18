1ce543d1f2c35b043bcb8e64c2a573a0a7dd02ea
Takeshi Iwana <tak@prismify.co>
2021-12-18 16:03:11 -0700

    Export gitly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export { default } from './core/gitly'
+import gitly from './core/gitly'
 export * from './core/gitly'
 export * from './core/download'
 export * from './core/extract'
 export * from './utils/url/parse'
 export * from './utils/url/git-url'
 export * from './types/git'
+export default gitly

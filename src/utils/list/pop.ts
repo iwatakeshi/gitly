e5cbe1da83c3b6f74435d4442ac0dbfe6f087b89
Takeshi <iwatakeshi@gmail.com>
2022-12-05 16:06:55 -0700

    Fix pop

--- src/utils/list/pop.ts
+++ src/utils/list/pop.ts
@@ -1,5 +1,7 @@
+import { ArrayLike } from '../../types/array-like'
 /**
  * An immutable `pop` function that removes the last item of a string or an array
  * @param x The string or array to pop
  */
-export const pop = <T = string>(x: (T[] | string)) => x.slice(0, x.length - 1) as (T extends string ? T : T[])
+export const pop = <T, U extends string | ArrayLike<T>>(x: U) =>
+  x.slice(0, x.length - 1)

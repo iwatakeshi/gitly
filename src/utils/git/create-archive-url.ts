f2264dc292be671effe02a9d95bf152378f784bf
Takeshi <iwatakeshi@gmail.com>
2022-12-05 16:08:57 -0700

    Update create-archive-url

--- src/utils/git/create-archive-url.ts
+++ src/utils/git/create-archive-url.ts
@@ -1,26 +1,38 @@
-import { cond, equals, T } from 'rambda'
 import { GitProvider } from '../../types/git'
 import { GitURL } from '../url/git-url'
 
-const isProvider = (provider: GitProvider) => (url: GitURL) =>
-  equals(url.provider, provider)
+export type GitURLResolver = (url: GitURL) => string
 
 /* istanbul ignore next */
-const toGitLab = ({repository, branch, pathname}: GitURL) =>
+const toGitLab: GitURLResolver = ({ repository, branch, pathname }: GitURL) =>
   `https://gitlab.com/${pathname}/-/archive/${branch}/${repository}-${branch}.tar.gz`
 
 /* istanbul ignore next */
-const toBitbucket = ({repository, branch}: GitURL) =>
+const toBitbucket: GitURLResolver = ({ repository, branch }: GitURL) =>
   `https://bitbucket.org/${repository}/get/${branch}.tar.gz`
 
 /* istanbul ignore next */
-const toGitHub = ({owner, repository, branch}: GitURL) =>
+const toGitHub: GitURLResolver = ({ owner, repository, branch }: GitURL) =>
   `https://github.com/${owner}/${repository}/archive/${branch}.tar.gz`
 
-export const createArchiveURL: (url: GitURL) => string = cond<GitURL, string>([
-  [isProvider('gitlab'), toGitLab],
-  [isProvider('bitbucket'), toBitbucket],
-  [T, toGitHub],
-])
+export function createArchiveURL<P extends string = never>(
+  url: GitURL,
+  resolvers?: Record<GitProvider<P>, GitURLResolver>
+): string {
+  const map = {
+    bitbucket: toBitbucket,
+    github: toGitHub,
+    gitlab: toGitLab,
+    ...resolvers,
+  } as Record<GitProvider<P>, GitURLResolver>
+
+  const resolver = Object.keys(map)
+    .map(provider => ({ provider, resolver: map[provider as GitProvider<P>] }))
+    .find(item => item.provider === url.provider)
+
+  if (resolver) return resolver.resolver(url)
+
+  return ''
+}
 
 export const $createArchiveURL = createArchiveURL

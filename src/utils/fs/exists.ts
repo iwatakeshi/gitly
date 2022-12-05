d352ff452f71a6c515f0947a5bc01a67239317cf
Takeshi <iwatakeshi@gmail.com>
2022-12-05 16:09:07 -0700

    Refactor exists

--- src/utils/fs/exists.ts
+++ src/utils/fs/exists.ts
@@ -1,13 +1,16 @@
 /* istanbul ignore file */
 
 import { constants, PathLike, promises as fs } from 'fs'
-import { tryCatchAsync } from "rambdax";
 
 /**
  * Determines whether a path exists
  * @param path The path to check
  */
-export const exists: (path: PathLike) => Promise<boolean> = tryCatchAsync(async (path) => {
-  await fs.access(path, constants.F_OK)
-  return true
-}, async () => false)
+export const exists: (path: PathLike) => Promise<boolean> = async (path) => {
+  try {
+    await fs.access(path, constants.F_OK)
+    return true
+  } catch (error) {
+    return false
+  }
+}

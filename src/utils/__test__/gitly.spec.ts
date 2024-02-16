e1a57a273951ca3d7c23d7e67d9b2a6bcf430fcf
Takeshi <tak@prismify.co>
2024-02-15 19:53:23 -0700

    Add a simple gitly test

---
+++ src/utils/__test__/gitly.spec.ts
@@ -0,0 +1,29 @@
+import { join } from "path";
+import gitly from "../gitly";
+import exists from "../exists";
+import { rm } from "shelljs";
+describe('gitly', () => {
+  const destination = join(__dirname, 'output', 'gitly', 'example')
+  const options = {
+    temp: join(__dirname, 'output', 'gitly', '.gitcopy'),
+  }
+
+  beforeEach(async () => {
+    rm('-rf', join(__dirname, 'output', 'gitly', '.gitcopy'))
+  })
+  afterEach(async () => {
+    rm('-rf', destination)
+  })
+
+  it('should clone the repository using axios', async () => {
+    const result = await gitly('lukeed/gittar', destination, options)
+    expect(await exists(result[0])).toBeDefined()
+    expect(await exists(result[1])).toBeDefined()
+  })
+
+  it('should clone the repository using git', async () => {
+    const result = await gitly('lukeed/gittar', destination, { ...options, backend: 'git' })
+    expect(await exists(result[0])).toBeDefined()
+    expect(await exists(result[1])).toBeDefined()
+  })
+});

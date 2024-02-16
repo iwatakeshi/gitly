656d2791024d03efce08a71ffa8a353e317a63f5
Takeshi <tak@prismify.co>
2024-02-15 19:57:00 -0700

    Added basic tests for clone

---
+++ src/utils/__test__/clone.spec.ts
@@ -0,0 +1,28 @@
+import exists from '../exists';
+import clone from '../clone'
+import { rm } from 'shelljs'
+import { join } from 'path';
+
+
+describe('utils/clone', () => {
+  const destination = join(__dirname, 'output', 'clone', 'example')
+  const options = {
+    temp: join(__dirname, 'output', 'clone', '.gitcopy'),
+  }
+
+  beforeEach(async () => {
+    rm('-rf', join(__dirname, 'output', 'clone', '.gitcopy'))
+  })
+  afterEach(async () => {
+    rm('-rf', destination)
+  })
+
+  afterAll(async () => {
+    rm('-rf', join(__dirname, 'output', 'clone', '.gitcopy'))
+  })
+  it('should clone the repository', async () => {
+    const result = await clone('lukeed/gittar', options)
+    console.log(result)
+    expect(await exists(result)).toBe(true)
+  })
+})
